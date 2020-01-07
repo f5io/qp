bf19d8c4cbfc37fa8b18bc1286e754c99df8483d
Joe Harlow <joe@paybase.io>
2020-01-07 13:22:07 +0000

    chore: add -J to tar

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "retrieving @paybase/csp@${CSP_VERSION}"
 curl "https://unpkg.com/@paybase/csp@$CSP_VERSION/dist/esm/index.js" > ./src/csp.js
 
 echo "retrieving quickjs@${QJS_VERSION}"
-mkdir -p qjs && curl "https://bellard.org/quickjs/quickjs-${QJS_VERSION}.tar.xz" | tar xf - -C qjs --strip-components 1
+mkdir -p qjs && curl "https://bellard.org/quickjs/quickjs-${QJS_VERSION}.tar.xz" | tar xJf - -C qjs --strip-components 1
 cd qjs && make
 
 
