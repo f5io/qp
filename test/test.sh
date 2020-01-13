c081b8225799c4c570718ae1dfbf4a141c6b5e72
Joe Harlow <joe@paybase.io>
2020-01-13 11:10:38 +0000

    chore: cleanup and store artifacts

--- test/test.sh
+++ test/test.sh
@@ -34,83 +34,83 @@ assert_silent_err() {
 
 assert \
  "select id where id >= 3 limit 2" \
- "{\"id\":3}\n{\"id\":4}" 
+ '{"id":3}\n{"id":4}' 
 
 assert \
   "select id as * where id >= 3 limit 3" \
-  "3\n4\n5"
+  '3\n4\n5'
 
 assert \
   "select data.0 as * limit 2" \
-  "\"heads\"\n\"tails\""
+  '"heads"\n"tails"'
 
 assert \
   "select age, name.first as firstName where age >= 40 limit 1" \
-  "{\"age\":40,\"firstName\":\"Sam\"}"
+  '{"age":40,"firstName":"Sam"}'
 
 assert \
   "select age as number offset 2 limit 2" \
-  "{\"number\":27}\n{\"number\":19}"
+  '{"number":27}\n{"number":19}'
 
 assert \
   "select date(dob) as birthYear limit 1" \
-  "{\"birthYear\":\"1979-06-23T00:00:00.000Z\"}"
+  '{"birthYear":"1979-06-23T00:00:00.000Z"}'
 
 assert \
   "select 1 limit 3" \
-  "1\n1\n1"
+  '1\n1\n1'
 
 assert \
   "select true limit 3" \
-  "true\ntrue\ntrue"
+  'true\ntrue\ntrue'
 
 assert \
   "select null limit 2" \
-  "null\nnull"
+  'null\nnull'
 
 assert \
   "select (1,2,3) limit 2" \
-  "[1,2,3]\n[1,2,3]"
+  '[1,2,3]\n[1,2,3]'
 
 assert \
   "select date(2020, 0) limit 1" \
-  "\"2020-01-01T00:00:00.000Z\""
+  '"2020-01-01T00:00:00.000Z"'
 
 assert \
   "select 1 as one limit 4" \
-  "{\"one\":1}\n{\"one\":1}\n{\"one\":1}\n{\"one\":1}"
+  '{"one":1}\n{"one":1}\n{"one":1}\n{"one":1}'
 
 assert \
   "select name.first as * where (age > 30 and age <= 40) or name.first = 'Orion' limit 4" \
-  "\"Sam\"\n\"Izzy\"\n\"Orion\"\n\"Cameron\""
+  '"Sam"\n"Izzy"\n"Orion"\n"Cameron"'
 
 assert \
   "select name.first as * where age > 30 and (age <= 40 or name.first = 'Orion') limit 3" \
-  "\"Sam\"\n\"Izzy\"\n\"Cameron\""
+  '"Sam"\n"Izzy"\n"Cameron"'
 
 assert \
   "select name.first as * where name.first like _am% limit 2" \
-  "\"Sam\"\n\"Cameron\""
+  '"Sam"\n"Cameron"'
 
 assert \
   "select id as * where id like 1 limit 10" \
-  "1\n10\n11\n12\n13\n14\n15\n16\n17\n18"
+  '1\n10\n11\n12\n13\n14\n15\n16\n17\n18'
 
 assert \
   "select name.first as * where name.first ilike '^[aeiou]' offset 1 limit 3" \
-  "\"Abed\"\n\"Orion\"\n\"Ana\""
+  '"Abed"\n"Orion"\n"Ana"'
 
 assert \
   "select id as * where id in (1,2,3) limit 3" \
-  "1\n2\n3"
+  '1\n2\n3'
 
 assert \
   "select id as * where 'heads' in data limit 2" \
-  "0\n5"
+  '0\n5'
 
 assert \
   "select from_entries(('f', name.first), ('nest', from_entries(('age', age)))) limit 1" \
-  "{\"f\":\"Sam\",\"nest\":{\"age\":40}}"
+  '{"f":"Sam","nest":{"age":40}}'
 
 assert_err \
   "{" \
@@ -118,11 +118,11 @@ assert_err \
   "json parse error: expecting property name"
 
 assert_err \
-  "{\"foo\":\"bar\"}\nfoobar\n{\"foo\":\"bar\"}" \
+  '{"foo":"bar"}\nfoobar\n{"foo":"bar"}' \
   "select true where foo = 'bar'" \
   "true\njson parse error: unexpected token: 'foobar'\ntrue"
 
 assert_silent_err \
-  "{\"foo\":\"bar\"}\nfoobar\n{\"foo\":\"bar\"}" \
+  '{"foo":"bar"}\nfoobar\n{"foo":"bar"}' \
   "select true where foo = 'bar'" \
   "true\ntrue"
